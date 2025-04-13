5bd37a816aef20afab8b52d0e8a40a5ded76bb3b
LouryNay <123560671+LouryNay@users.noreply.github.com>
2025-04-13 16:48:37 +0200

    new library opencv

--- opencv/build/intermediates/cxx/Debug/5o396w4h/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\melin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\melin\\AndroidStudioProjects\\DocScanner\\opencv\\.cxx\\Debug\\5o396w4h\\arm64-v8a" ^
-  opencv_jni_shared
